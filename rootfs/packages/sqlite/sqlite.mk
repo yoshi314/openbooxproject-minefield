692910c9e08243abc232758b22697051e7e9c0ae
yoshi314 <yoshi314@gmail.com>
2013-10-01 18:01:45 +0200

    update sqlite ; add sqlite3 command line tool

--- rootfs/packages/sqlite/sqlite.mk
+++ rootfs/packages/sqlite/sqlite.mk
@@ -1,9 +1,9 @@
-SQLITE_SOURCE=sqlite-amalgamation-3080001.zip
-SQLITE_DIR=$(BUILD_DIR)/sqlite-amalgamation-3080001
+SQLITE_SOURCE=sqlite-amalgamation-3080002.zip
+SQLITE_DIR=$(BUILD_DIR)/sqlite-amalgamation-3080002
 
 $(DL_DIR)/$(SQLITE_SOURCE):
 #	$(WGET) -P $(DL_DIR) $(DOWNLOAD_SITE)/$(SQLITE_SOURCE)
-	$(WGET) -P $(DL_DIR) http://www.sqlite.org/2013/sqlite-amalgamation-3080001.zip
+	$(WGET) -P $(DL_DIR) http://www.sqlite.org/2013/sqlite-amalgamation-3080002.zip
 
 $(SQLITE_DIR)/.unpacked:$(DL_DIR)/$(SQLITE_SOURCE)
 	mkdir -p $(SQLITE_DIR)
@@ -16,18 +16,30 @@ $(SQLITE_DIR)/libsqlite3.so.0.8.6:$(SQLITE_DIR)/.unpacked
 	(cd $(SQLITE_DIR);ln -s libsqlite3.so.0.8.6 libsqlite3.so.0)
 	(cd $(SQLITE_DIR);ln -s libsqlite3.so.0.8.6 libsqlite3.so)
 
+$(SQLITE_DIR)/sqlite3:$(SQLITE_DIR)/libsqlite3.so.0.8.6
+	(cd $(SQLITE_DIR);$(TARGET_CC) sqlite3.h shell.c sqlite3.o -o sqlite3 -lpthread -ldl)
+
+
 $(HOST_DIR)$(EPREFIX)/lib/libsqlite3.so.0.8.6:$(SQLITE_DIR)/libsqlite3.so.0.8.6
 	cp -dpf $(SQLITE_DIR)/sqlite3.h $(HOST_DIR)$(EPREFIX)/include
 	cp -dpf $(SQLITE_DIR)/sqlite3ext.h $(HOST_DIR)$(EPREFIX)/include
 	cp -dpf $(SQLITE_DIR)/libsqlite3.so* $(HOST_DIR)$(EPREFIX)/lib
 	touch -c $(HOST_DIR)$(EPREFIX)/lib/libsqlite3.so.0.8.6
 
+$(HOST_DIR)$(EPREFIX)/bin/sqlite3:$(SQLITE_DIR)/sqlite3
+	cp -dpf $(SQLITE_DIR)/sqlite3 $(HOST_DIR)$(EPREFIX)/bin
+
 $(TARGET_DIR)$(EPREFIX)/lib/libsqlite3.so.0.8.6:$(HOST_DIR)$(EPREFIX)/lib/libsqlite3.so.0.8.6
 	cp -dpf $(HOST_DIR)$(EPREFIX)/lib/libsqlite3.so* $(TARGET_DIR)$(EPREFIX)/lib
 	-$(TARGET_STRIP) $(TARGET_DIR)$(EPREFIX)/lib/libsqlite3.so.0.8.6
 	touch -c $(TARGET_DIR)$(EPREFIX)/lib/libsqlite3.so.0.8.6
 
-sqlite: $(TARGET_DIR)$(EPREFIX)/lib/libsqlite3.so.0.8.6
+$(TARGET_DIR)$(EPREFIX)/bin/sqlite3:$(HOST_DIR)$(EPREFIX)/bin/sqlite3
+	cp -dpf $(HOST_DIR)$(EPREFIX)/bin/sqlite3 $(TARGET_DIR)$(EPREFIX)/bin
+
+sqlite: $(TARGET_DIR)$(EPREFIX)/lib/libsqlite3.so.0.8.6 $(TARGET_DIR)$(EPREFIX)/bin/sqlite3
+
+sqlite-app: $(SQLITE_DIR)/sqlite3
 
 sqlite-source:$(DL_DIR)/$(SQLITE_SOURCE)
 
